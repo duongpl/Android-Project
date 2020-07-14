a0e0e8ddde652b11ed132613004eca087c354898
duongpl <51127228+duongpl@users.noreply.github.com>
2020-07-15 00:10:01 +0700

    update

---
+++ app/src/main/java/fu/prm391/sample/navigation/adapter/FoodAdapter.java
@@ -0,0 +1,66 @@
+package fu.prm391.sample.navigation.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+import java.time.Instant;
+import java.util.ArrayList;
+
+import fu.prm391.sample.navigation.R;
+import fu.prm391.sample.navigation.model.Food;
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder>{
+
+    private Context context;
+    private ArrayList<Food> food;
+
+    public FoodAdapter(Context mContext, ArrayList<Food> foods) {
+        this.context = mContext;
+        this.food = foods;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView img_food;
+        private TextView name_food;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img_food = itemView.findViewById(R.id.image);
+            name_food = itemView.findViewById(R.id.text_name);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View heroView = inflater.inflate(R.layout.item_image, parent, false);
+        ViewHolder viewHolder = new ViewHolder(heroView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Food f = food.get(position);
+        Picasso.get()
+                .load(f.getImg())
+                .into(holder.img_food);
+        holder.name_food.setText(f.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return food.size();
+    }
+
+}
---
+++ app/src/main/java/fu/prm391/sample/navigation/model/Food.java
@@ -0,0 +1,49 @@
+package fu.prm391.sample.navigation.model;
+
+public class Food {
+    private String id;
+    private String name;
+    private String img;
+    private String category_type;
+
+    public Food(){}
+
+    public Food(String id, String name, String img, String category_type) {
+        this.id = id;
+        this.name = name;
+        this.img = img;
+        this.category_type = category_type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getCategory_type() {
+        return category_type;
+    }
+
+    public void setCategory_type(String category_type) {
+        this.category_type = category_type;
+    }
+}
